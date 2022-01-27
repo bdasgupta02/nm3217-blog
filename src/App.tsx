51b6da8ce80ce00a256ae83421964ed71b5d53f1
bdasgupta02 <bdasgupta02@gmail.com>
2022-01-27 16:51:06 +0800

    added page wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+
+import PageWrapper from './components/PageWrapper';
+const Placeholder = () => (<PageWrapper><div>This is a placeholder page</div><div style={{ width: '1px', height: '3000px' }}></div></PageWrapper>)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div style={{ flexGrow: 1, display: 'flex', height: '100vh' }}>
+        <Routes>
+
+          {/* Add pages here with Routes */}
+          <Route path="/assignments" element={<Placeholder />} />
+          <Route path="/assignment/:id" element={<Placeholder />} />
+          <Route path="/lecture_exercises" element={<Placeholder />} />
+          <Route path="/lecture_exercise/:id" element={<Placeholder />} />
+          <Route path="/final_project" element={<Placeholder />} />
+
+          {/* Keep the last one for the home page */}
+          <Route path="/" element={<Placeholder />} />
+
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/PageWrapper/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Box, Container, Grid } from '@mui/material'
+import './pageWrapper.css'
+
+
+/**
+ * PageWrapper
+ * - Wrap all your pages inside this component.
+ * - Responsive and simple.
+ * 
+ * Alternatives
+ * - Grid-component based system [not chosen due to shit responsiveness compared to this]
+ */
+
+
+type Props = {
+    title?: string,
+    subtitle?: string,
+    children: React.ReactNode
+}
+
+const defaultProps: Props = {
+    title: '',
+    subtitle: '',
+    children: <div></div>
+}
+
+const PageWrapper = (props: Props) => {
+    const { title, subtitle, children } = { ...defaultProps, ...props }
+
+    return (
+        <div id="PWOuter">
+            <Container maxWidth="lg">
+                <Box minHeight="100vh" sx={{ paddingTop: '100px', paddingBottom: '100px' }}>
+                    <div style={{ width: '100%' }}>
+                        <div>{title}</div>
+                    </div>
+                    {children}
+                </Box>
+            </Container>
+        </div>
+    )
+}
+
+export default PageWrapper
