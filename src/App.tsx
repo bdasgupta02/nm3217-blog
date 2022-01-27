078c088194a29fa56be45b152555828e7c6d0dc7
bdasgupta02 <bdasgupta02@gmail.com>
2022-01-28 04:32:15 +0800

    test cache

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ const Placeholder = () => (<PageWrapper><div>This is a placeholder page</div><di
 function App() {
   return (
     <Router>
+      
       <div style={{ flexGrow: 1, display: 'flex', height: '100vh' }}>
+      <div style={{ backgroundColor: '#000', minHeight: '100vh', width: '40px', overflowY: 'auto' }} />
         <Routes>
 
           {/* Add pages here with Routes */}
